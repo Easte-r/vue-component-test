12d2731aa49b5b975681aa8d92a9288fa898a61d
zhendong ren <zhendong ren>
2019-03-11 22:06:32 +0800

    '初始化组件'

---
+++ child.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <button @click="ChildEvent">{{name}}</button>
+</div>
+    
+    
+</template>
+
+<script>
+export default {
+    
+    data() {
+        return {
+            btninfo: '点击我',
+        }
+    },
+    props: {
+        name: String
+    },
+    mounted() {
+
+    },
+    methods: {
+        ChildEvent() {
+            this.$emit('events',{a:1,b:2},'haha')
+        }
+    }
+    
+
+}
+</script>
---
+++ test-component.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <h1>哈哈哈哈哈哈哈</h1>
+    <p>{{message}}</p>
+    <child name="我是子组件" @events="FatherEvent"></child>
+</div>
+    
+    
+</template>
+
+<script>
+import child from './child.vue'
+export default {
+    
+    data() {
+        return {
+            message: 1,
+        }
+    },
+    mounted() {
+        // alert(12321)
+    },
+
+    methods: {
+        FatherEvent(e,one) {
+            // alert('father');
+            /* eslint-disable */
+            console.log(e, one);
+        }
+    },
+
+    components: {
+        child: child
+    }
+}
+</script>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
